8e22f58d521bee5ed5d6c597faad7566441141ff
tetrovolt <raymond@sutrisno.me>
2017-01-26 16:43:36 -0600

    added kinematics

---
+++ src/kinematics.py
@@ -0,0 +1,11 @@
+
+from math import cos, sin, sqrt
+from vectorstuff import Vector
+
+'''
+Plan so far:
+	-> Implement position, velocity, and acceleration as vectors
+	-> Have a big Kinematic equation class
+	-> Perhaps a smaller 1D kinematic equation class
+
+'''
